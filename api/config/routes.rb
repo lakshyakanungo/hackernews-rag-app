b33ab4dce67c1024d889e2d9096cace1b8f8ddee
Lakshya Kanungo <lakshyaislearning@gmail.com>
2025-11-03 09:51:59 +0530

    Added route for pinging

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # Define your API routes under a namespace to version them.
   # This creates the URL: /api/v1/chat
   namespace :api do
     namespace :v1 do
       get "chat/stream", to: "chat#stream"
     end
   end
+  get :ping, to: "ping#show"
 end
